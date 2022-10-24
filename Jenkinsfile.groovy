f0ca2c551e82c5d0cd7132655ceecff24998e9fa
JustMisty <102553988+JustMisty@users.noreply.github.com>
2022-10-24 22:51:19 +0400

    add Artefact and Deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,7 +7,33 @@ pipeline {
                 bat "build.bat"
             }
         }
-      
+      stage('test') {
+            steps {
+                bat "buildTest.bat"
+            }
+        }
+	    
+	    stage('Archive'){
+                steps{
+			dir('C:\\'){
+				echo "Current build: ${BUILD_NUMBER}"
+				zip zipFile: "${BUILD_NUMBER}.zip", archive:false, dir: 'ProgramData\\Jenkins\\.jenkins\\workspace\\Job228\\Calc\\bin\\Debug\\netcoreapp3.1'
+				archiveArtifacts artifacts: "${BUILD_NUMBER}.zip"
+			}
+		  }
+    	}
+	    stage('Deploy'){
+		  steps{
+			  dir('C:\\'){
+				  script{
+					  try
+					  {
+						  bat("md C:\\Deploy\\")
+					  }catch(Exception e){}
+				  }
+				  unzip zipFile: "${BUILD_NUMBER}.zip", dir: 'C:\\Deploy'
+			  }
+		  }
  }
 	    
     }
